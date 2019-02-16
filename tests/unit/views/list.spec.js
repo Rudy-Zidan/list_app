a0fc4b5ac96f31cc7a0c0a99c2f3cde47b1c1035
Rudy Zidan <rz.zidan@gmail.com>
2019-02-16 17:01:16 +0200

    update list spec

--- tests/unit/views/list.spec.js
+++ tests/unit/views/list.spec.js
@@ -1,7 +1,7 @@
 import { shallowMount } from "@vue/test-utils";
 import List from "@/views/List.vue";
 
-describe("Trash.vue", () => {
+describe("List.vue", () => {
   it("render component", () => {
     const wrapper = shallowMount(List);
     expect(wrapper.find("h1").text()).toBe("This is Lists page");
